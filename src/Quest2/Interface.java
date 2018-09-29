ebc37daacfa063d7599498614c0ff3ace701a91b
hilderjares <hider1195@gmail.com>
2018-09-28 23:45:48 -0300

    Updade Interface.java

--- src/Quest2/Interface.java
+++ src/Quest2/Interface.java
@@ -1,45 +1,44 @@
 package Quest2;
 
 public class Interface {
-
     public static void main(String[] args) {
-        Miseravel ms1 = new Miseravel();
-        ms1.setNome("Valdete");
-        ms1.setIdade(20);
-        ms1.mendiga = true;
-        String nomem = ms1.getNome();
-        int idadem = ms1.getIdade();
-        System.out.println("Nome pessoa miseravel: " + nomem);
-        System.out.println("Idade pessoa miseravel: " + idadem);
-        System.out.println("Mendiga: " + ms1.mendiga);
+        Miseravel m1 = new Miseravel();
+        Pobre p1 = new Pobre();
+        Rica r1 = new Rica();
+        
+        m1.setNome("Valdete");
+        m1.setIdade(20);
+        m1.mendiga = true;
+        String nomeM = m1.getNome();
+        int idadeM = m1.getIdade();
+        System.out.println("Nome pessoa miseravel: " + nomeM);
+        System.out.println("Idade pessoa miseravel: " + idadeM);
+        System.out.println("Mendiga: " + m1.mendiga);
         
         System.out.println("***********");
 
-        Pobre pobre1 = new Pobre();
-        pobre1.setNome("Tainara");
-        pobre1.setIdade(16);
-        pobre1.trabalha = false;
-        String nomep = pobre1.getNome();
-        int idadep = pobre1.getIdade();
-        System.out.println("Nome pessoa pobre:" + nomep);
-        System.out.println("Idade pessoa pobre:" + idadep);
-        System.out.println("Trabalha:" + pobre1.trabalha);
+        p1.setNome("Tainara");
+        p1.setIdade(16);
+        p1.trabalha = false;
+        String nomeP = p1.getNome();
+        int idadeP = p1.getIdade();
+        System.out.println("Nome pessoa pobre:" + nomeP);
+        System.out.println("Idade pessoa pobre:" + idadeP);
+        System.out.println("Trabalha:" + p1.trabalha);
         
         System.out.println("***********");
         
-        Rica rica1 = new Rica();
-        rica1.setNome("Leticia");
-        rica1.setIdade(16);
-        rica1.comprar = true;
-        rica1.setDinheiro(1000);
-        String nomeR = rica1.getNome();
-        int idadeR = rica1.getIdade();
-        double Dinheiro = rica1.getDinheiro();
+        r1.setNome("Leticia");
+        r1.setIdade(16);
+        r1.comprar = true;
+        r1.setDinheiro(1000);
+        String nomeR = r1.getNome();
+        int idadeR = r1.getIdade();
+        double dinheiro = r1.getDinheiro();
         System.out.println("Nome Rica: "+nomeR);
         System.out.println("Idade Rica: "+idadeR);
-        System.out.println("Comprar: "+rica1.comprar);
-        System.out.println("Dinheiro: "+Dinheiro);
+        System.out.println("Comprar: "+r1.comprar);
+        System.out.println("Dinheiro: "+dinheiro);
         
     }
-
 }
