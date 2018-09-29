75fc20272b4efb961a383c69d6a666c566477dac
hilderjares <hider1195@gmail.com>
2018-09-28 23:39:06 -0300

    Update Pobre.java

--- src/Quest2/Pobre.java
+++ src/Quest2/Pobre.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Quest2;
 
-public class Pobre extends Pessoa{
+public class Pobre extends Pessoa {
     boolean trabalha;
-    public boolean trabalha(){
+    
+    public boolean trabalha() {
         return trabalha;
     }
 }
